69cb540c67a33a66683f630035aa538b3065d712
Darren Chong <chongjinheng@hotmail.com>
2026-06-23 16:42:09 +0800

    chore: remove unused GITHUB_PAT from deploy wiring
    Only FRIDAY_PAT is needed for gh CLI auth. GITHUB_PAT was never
    used by any service.

--- modules/deploy.sh
+++ modules/deploy.sh
@@ -176,7 +176,6 @@ if $GITHUB_MODE || check_file "$HERMES_ENV"; then
 
   # Optional
   echo "  [Optional]"
-  check_var_optional "GITHUB_PAT" "$HERMES_ENV"
   check_var_optional "FRIDAY_PAT" "$HERMES_ENV"
   check_var_optional "BRAVE_SEARCH_API_KEY" "$HERMES_ENV"
   check_var_optional "FIRECRAWL_API_KEY" "$HERMES_ENV"
